449461076f6852782ffeda241444ea4a271af055
cc23158 - Victor <vymimura2@gmail.com>
2025-09-30 10:14:11 -0300

    initial configurations

---
+++ front/src/components/sections/Header.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import { useFormState } from "react-dom";
+import { FaWhatsapp, FaLinkedin, FaGithub, FaSun, FaMoon } from "react-icons/fa"
+
+const Header = () => {
+    cont [isDarkMode, setIsDarkMode] = useFormState()
+
+    return (
+        <div>
+            <h1 className="font-monospace">asdasd</h1>
+        </div>
+    );
+}
+
+export default Header;
